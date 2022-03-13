bc2fb04b08c5e07f9468f1f0782414d153e8d376
n-premasinghe <99045891+n-premasinghe@users.noreply.github.com>
2022-03-13 19:46:22 -0400

    Added Comments
    Comments detailing specific functions added.

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+# Importing libraries
 import pandas as pd
 import numpy as np
 
+# Reading data stored in spreadsheets.
 snowDaysRaw = pd.read_excel('SCSTC_SchoolBus_user_tweets.xlsx')
 weatherData = pd.read_csv('weather_data_24hr.csv')
 snowDaysRaw.head()
 
+# Sorting to make more readable.
 snowDaysRaw.sort_index(axis=1)
 
+# function to turn raw UTC data into weather data compatible date format.
 def dateClean(text):
-    #start = text.find('20')
+    # Determining whether or not the index value contains "20" by seeing if the position of the "-" is in the right place.
     if text[text.find('20') + 4] != '-':
+        # In the case that the index value of the date contains "20" add 2 to the index position to pass it.
         trueText = text[text.find('20') + 2:]
+        # Perform normal procedure on edited text. 
+        # (Note: This implementation is a quick fix I put in place knowing that there were no where near 
+        # #2000 datapoints within the set. If there were, a simple loop could solve the problem by consistently checking to see if the "-" is in the right place.)
         start = trueText.find('20')
     else:
         start = text.find('20')
@@ -18,15 +26,27 @@ def dateClean(text):
     end = text.find('T')
     
     text = text[start:end]
+    # Formatting the date so .loc commands can be run on the weatherData dataframe.
     text = text.replace("-", "/")
-        
+
+    # Returns cleaned date.    
     return text
 
+# 
+#   Extracting tweets that show there was a snow day on that date.
+#
 
+# Keywords to look for. (Two lists need to be used to account for times all zones have a snow day, and days when it is just the west zone.)
 keywordsWest = ['cancelled', 'West']
 keywordsAll = ['cancelled', 'All', 'Simcoe County']
+
+# List containing the text of the tweets that have the keywords in them.
 snowDayTweets = []
+# List containing the dates that the tweets with the keywords were made. (Tuples were intentionally not used to take advantage of the simplicity of the zip() function
+# when translating the values to dataframes later.)
 snowDayTweetDates = []
+
+# Iterating through every tweet to look for snow days
 for t in snowDaysRaw['Text']:
     if all(x in t for x in keywordsWest):
         snowDayTweets.append(t)
@@ -41,7 +61,8 @@ for t in snowDaysRaw['Text']:
         tweetDate = str(snowDaysRaw.loc[snowDaysRaw['Text'] == t, 'UTC'])
         print(dateClean(tweetDate))
         snowDayTweetDates.append(dateClean(tweetDate))
-        
+
+
 print(len(snowDayTweets))
 print(len(snowDayTweetDates))
 
